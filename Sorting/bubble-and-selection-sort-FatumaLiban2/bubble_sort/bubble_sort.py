3522e04087182edcb3f5fe41a1f9a5c5490f43fe
FatumaLiban2 <fatuma.omar@strathmore.edu>
2025-06-20 13:16:45 +0300

    Add files via upload

---
+++ Sorting/bubble-and-selection-sort-FatumaLiban2/bubble_sort/bubble_sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(unsorted_list):
+    # TODO: Implement bubble sort
+    n= len(unsorted_list)
+    for i in range(n):
+        for j in range(0,5):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+    return unsorted_list
+
+
+
+                                                         
